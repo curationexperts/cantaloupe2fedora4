aad71706f09838c0a2de368f846df8d20e8ab144
Bess Sadler <bess@curationexperts.com>
2018-03-07 18:55:15 -0500

    Fetch image URL from Fedora4
    Given a fileset id, get the fileset object from Fedora4.
    Return the fileset's hasFile object.
    Test with rspec, use vcr to record HTTP interactions.
    Check style with Bixby.
    Use JRuby, which is required by Cantaloupe.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source 'https://rubygems.org'
 gem 'bixby', '~> 1.0' # bixby == the samvera community's rubocop rules
+gem 'dotenv'
+gem 'ldp'
 gem 'rspec'
+gem 'vcr'
+gem 'webmock'
--- spec/http_resolver_spec.rb
+++ spec/http_resolver_spec.rb
@@ -1,8 +1,25 @@
 $LOAD_PATH << '../lib'
 require 'delegates.rb'
+require 'vcr'
+require 'webmock'
+
+ENV['FEDORA_URL'] = 'http://127.0.0.1:8984/rest/dev/'.freeze
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
 
 describe Cantaloupe::HttpResolver do
-  it 'accepts an id and returns a url' do
-    expect(Cantaloupe::HttpResolver.get_url('abc123', nil)).to eq 'https://fedora.info/blah'
+  it 'gets the fedora url' do
+    expect(described_class.fedora_url).to eq 'http://127.0.0.1:8984/rest/dev/'
+  end
+  it 'gets the fileset url' do
+    expect(described_class.fileset_url('9g54xh65x')).to eq 'http://127.0.0.1:8984/rest/dev/9g/54/xh/65/9g54xh65x'
+  end
+  it 'gets the file url' do
+    VCR.use_cassette("fedora4query") do
+      expect(described_class.get_url('9g54xh65x', nil)).to eq 'http://127.0.0.1:8984/rest/dev/9g/54/xh/65/9g54xh65x/files/83ef8308-289e-4108-b30d-8640ce9e2e5f'
+    end
   end
 end
